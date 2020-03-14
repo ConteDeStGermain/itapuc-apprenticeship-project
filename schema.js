937a2da40f791a0fff1bf721938871e3d4b61b65
Justin T <itapuc7@gmail.com>
2020-03-14 11:14:53 -0400

    Added schemas

--- schema.js
+++ schema.js
@@ -1,4 +1,3 @@
-
 module.exports.userSchema = {
     type: "object",
   
@@ -14,6 +13,49 @@ module.exports.userSchema = {
       email: {
         type: "string",
         pattern: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/
+      },
+    },
+  };
+
+  module.exports.roomSchema = {
+    type: "object",
+  
+    required: ['createdAt', 'participants'],
+    properties: {
+      createdAt: {
+        bsonType: "Date",
+        type: "string",
+      },
+      lastMessage: {
+        bsonType: 'ObjectId',
+        type: "string",
+      },
+      participants: {
+        type: 'array',
+        uniqueItems: true,
+        items: {
+            bsonType: 'ObjectId',
+            type: 'string'
+        }
+      },
+    },
+  };
+
+  module.exports.messageSchema = {
+    type: "object",
+  
+    required: ['createdAt', 'authorId', 'text'],
+    properties: {
+      createdAt: {
+        bsonType: "Date",
+        type: "string",
+      },
+      authorId: {
+        bsonType: 'ObjectId',
+        type: "string",
+      },
+      text: {
+        type: 'string',
       },
     },
   };
