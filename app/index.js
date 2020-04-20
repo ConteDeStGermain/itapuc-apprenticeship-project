63aa28a83fa521877611c3acedc35ec84c56ca91
Jamie Bertram <jamie.bertram@acadium.com>
2020-04-20 13:05:01 -0400

    add app state

--- app/index.js
+++ app/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// App state:
+// messages: an object which maps room IDs to arrays of messages
+// rooms: an array of rooms for the logged in user
+// session: an object that describes the currently logged in user. Has two keys:
+// token and user
+// users: an array of users
+
 function App() {
+  const [messages, setMessages] = useState({});
+  const [rooms, setRooms] = useState([]);
+  const [session, setSession] = useState();
+  const [users, setUsers] = useState([]);
+
   return <div>Write the app here</div>;
 }
 
