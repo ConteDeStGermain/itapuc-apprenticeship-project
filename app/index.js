b880cae6ec6263432a33e71b4ed9c80cff5cd939
Jamie Bertram <jamie.bertram@acadium.com>
2020-04-20 11:57:41 -0400

    add react boilerplate

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>Write the app here</div>;
+}
+
+const appNode = document.getElementById("app");
+ReactDOM.render(<App />, appNode);
